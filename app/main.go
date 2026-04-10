29657338e4fe441bc2e7b08b7fd208d188fd9b2c
BoundlessWay <70361753+BoundlessWay@users.noreply.github.com>
2026-04-10 11:18:59 +0700

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -6,25 +6,43 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	
+	fmt.Println("Redis server starting...")
 
-	// Uncomment the code below to pass the first stage
-	//
+	// 1. Bind port to listen for incoming connections
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
+
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	
+	defer l.Close()
+
+	// 2. Accept connection from a client
 	conn, err := l.Accept()
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Write([]byte("+PONG\r\n"))
+
+	defer conn.Close()
+
+	// 3. Handle client request (for simplicity, we just respond with PONG)
+	for {
+		buf := make([]byte, 1024)
+
+		// Read data from the socket
+		_, err := conn.Read(buf)
+
+		if err != nil {
+			fmt.Println("Connection closed or error encountered.")
+			break 
+		}
+
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
